fe2501a7a66a35f513fc3c117883e0b7038def1b
Remco de Boer <redeboer@gmx.com>
2021-05-27 13:33:52 +0200

    feat: provide mapping for numpy as backend

--- phasespace/backend/__init__.py
+++ phasespace/backend/__init__.py
@@ -13,10 +13,13 @@ __all__ = [
 
 class BackendType(Enum):
     TENSORFLOW = auto()
+    NUMPY = auto()
 
     @staticmethod
     def get_backend(backend: str) -> "BackendType":
         backend_formatted = backend.lower().strip()
+        if backend_formatted in {"np", "numpy"}:
+            return BackendType.NUMPY
         if backend_formatted in {"", "tf", "tensorflow"}:
             return BackendType.TENSORFLOW
         raise NotImplementedError(f'No backend implemented for "{backend}"')
@@ -57,3 +60,22 @@ if BACKEND == BackendType.TENSORFLOW:
 
     is_eager = bool(os.environ.get("PHASESPACE_EAGER"))
     tf.config.run_functions_eagerly(is_eager)
+
+if BACKEND == BackendType.NUMPY:
+    import numpy as tnp
+
+    from . import _np_random as random
+
+    function = lambda x: x
+    function_jit = lambda x: x
+    function_jit_fixedshape = lambda x: x
+
+    Tensor = tnp.ndarray
+    Variable = tnp.ndarray
+    get_shape = tnp.shape
+
+    def assert_equal(x, y, message: str, name: str = "") -> None:
+        return tnp.testing.assert_equal(x, y, err_msg=message)
+
+    def assert_greater_equal(x, y, message: str, name: str = "") -> None:
+        return tnp.testing.assert_array_less(-x, -y, err_msg=message)
